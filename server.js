b3ca849a2a73daa357fd44005ee959f026e5cb74
Felistas <felistaswaceera@gmail.com>
2018-10-12 12:59:06 +0300

    Add a database connection

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const dbConfig = require('./config/database.config');
+const mongoose = require('mongoose');
 const app = express();
 
 //parse requests of content-type json and application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+
+//connect to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser:true
+}).then(()=>{
+    console.log("Database successfully connected");
+}).catch((err)=>{
+    console.log("Error connecting to the database", err);
+    process.exit();
+})
 
 //simple test route
 app.get('/', (req, res) =>{
